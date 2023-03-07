af18ca9c9318aa9b9a729e369f0f3af1fb6c0bc6
Yash Powar <123502765+Yash-Powar@users.noreply.github.com>
2023-03-07 16:41:37 +0530

    Routes Added

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require('path')
 const app = express()
 
 
-dotenv.config({path:'config.env'})
+dotenv.config({ path: 'config.env' })
 const PORT = process.env.PORT || 8080
 
 
@@ -16,15 +16,11 @@ const PORT = process.env.PORT || 8080
 
 app.use(morgan('tiny'));
 
-app.get('/',(req,res)=>{
-    // res.send("Crud Application ");
-    res.render('index.ejs')
-})
 
 
 //parse request to body parser
 
-app.use(bodypaser.urlencoded({extended:true}))
+app.use(bodypaser.urlencoded({ extended: true }))
 
 
 //set view engine
@@ -34,11 +30,30 @@ app.set("view engine", "ejs")
 
 //load assets
 
-app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
-app.use('/img',express.static(path.resolve(__dirname,"assets/img")))
-app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
+
+app.get('/', (req, res) => {
+    // res.send("Crud Application ");
+    res.render('index')
+})
+
+//route for adding user into form
+
+app.get('/add-user', (req, res) => {
+
+    res.render('add_user')
+})
+
+//route for updating user 
+
+app.get('/update-user', (req, res) => {
+
+    res.render('update_user')
+})
 
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server is Running on http://localhost:${PORT}`);
 })
